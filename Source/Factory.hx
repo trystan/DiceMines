601ed2ca985faca3397d7b806f9efc5cb9b942c3
Trystan Spangler <trystan.s@comcast.net>
2014-03-09 15:22:49 -0700

    add several types of each item

--- Source/Factory.hx
+++ Source/Factory.hx
@@ -38,22 +38,53 @@ class Factory {
     }
 
     public static function rangedWeapon():Item {
-        var i = new Item(")", Color.hsv(30, 80, 80), "ranged", "short bow");
-        i.accuracyStat = "1d2+0";
-        i.damageStat = "1d2+0";
+        var list = [{ name:"crappy bow",    accuracy:"0d0+0", evasion:"0d0+0", damage:"0d0+0", resistance:"0d0+0"},
+                    { name:"short bow",     accuracy:"2d2+0", evasion:"0d0+0", damage:"1d1+0", resistance:"0d0+0"},
+                    { name:"medium bow",    accuracy:"1d2+0", evasion:"0d0+0", damage:"1d2+0", resistance:"0d0+0"},
+                    { name:"long bow",      accuracy:"1d1+0", evasion:"0d0+0", damage:"2d2+0", resistance:"0d0+0"},
+                    { name:"silver bow",    accuracy:"1d1+5", evasion:"0d0+0", damage:"1d1+5", resistance:"0d0+0"},
+                    { name:"heroic bow",    accuracy:"1d1+1", evasion:"0d0+1", damage:"1d1+1", resistance:"0d0+1"}];
+
+        var def = list[Math.floor(Math.random() * list.length)];
+        var i = new Item(")", Color.hsv(30, 80, 80), "ranged", def.name);
+        i.accuracyStat = def.accuracy;
+        i.evasionStat = def.evasion;
+        i.damageStat = def.damage;
+        i.resistanceStat = def.resistance;
         return i;
     }
 
     public static function meleeWeapon():Item {
-        var i = new Item("|", Color.hsv(200, 20, 80), "melee", "short sword");
-        i.accuracyStat = "1d2+0";
-        i.damageStat = "1d2+0";
+        var list = [{ name:"crappy sword",    accuracy:"0d0+0", evasion:"0d0+0", damage:"0d0+0", resistance:"0d0+0"},
+                    { name:"short sword",     accuracy:"2d2+0", evasion:"0d0+0", damage:"1d1+0", resistance:"0d0+0"},
+                    { name:"medium sword",    accuracy:"1d2+0", evasion:"0d0+0", damage:"1d2+0", resistance:"0d0+0"},
+                    { name:"long sword",      accuracy:"1d1+0", evasion:"0d0+0", damage:"2d2+0", resistance:"0d0+0"},
+                    { name:"silver sword",    accuracy:"1d1+5", evasion:"0d0+0", damage:"1d1+5", resistance:"0d0+0"},
+                    { name:"heroic sword",    accuracy:"1d1+1", evasion:"0d0+1", damage:"1d1+1", resistance:"0d0+1"}];
+        
+        var def = list[Math.floor(Math.random() * list.length)];
+        var i = new Item("|", Color.hsv(180, 20, 80), "melee", def.name);
+        i.accuracyStat = def.accuracy;
+        i.evasionStat = def.evasion;
+        i.damageStat = def.damage;
+        i.resistanceStat = def.resistance;
         return i;
     }
 
     public static function armor():Item {
-        var i = new Item("[", Color.hsv(60, 50, 80), "armor", "light armor");
-        i.resistanceStat = "1d1+0";
+        var list = [{ name:"crappy armor",    accuracy:"0d0+0", evasion:"0d0+0", damage:"0d0+0", resistance:"0d0+0"},
+                    { name:"light armor",     accuracy:"0d0+0", evasion:"2d2+0", damage:"0d0+0", resistance:"1d1+0"},
+                    { name:"medium armor",    accuracy:"0d0+0", evasion:"1d2+0", damage:"0d0+0", resistance:"1d2+0"},
+                    { name:"heavy armor",     accuracy:"0d0+0", evasion:"1d1+0", damage:"0d0+0", resistance:"2d2+0"},
+                    { name:"silver armor",    accuracy:"0d0+0", evasion:"1d1+5", damage:"0d0+0", resistance:"1d1+5"},
+                    { name:"heroic armor",    accuracy:"0d0+1", evasion:"1d1+1", damage:"0d0+1", resistance:"0d0+1"}];
+
+        var def = list[Math.floor(Math.random() * list.length)];
+        var i = new Item("[", Color.hsv(90, 40, 80), "armor", def.name);
+        i.accuracyStat = def.accuracy;
+        i.evasionStat = def.evasion;
+        i.damageStat = def.damage;
+        i.resistanceStat = def.resistance;
         return i;
     }
 
